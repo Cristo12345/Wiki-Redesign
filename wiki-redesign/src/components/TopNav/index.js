5bf630a99b20a76b67da906c6f7293535c07b1bb
Christian Suleiman <cisuleiman@gmail.com>
2020-01-21 16:38:13 -0500

    added wikipedia logo svg file

--- wiki-redesign/src/components/TopNav/index.js
+++ wiki-redesign/src/components/TopNav/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import {Navbar} from "react-bootstrap";
 import "./style.css";
+import WikipediaLogo from "./WikipediaLogo.svg";
+
 
 const TopNav = () => {
     return(
-        <Navbar bg="dark" expand="lg">
+        <Navbar bg="dark" expand="lg" className="Navbar-custom">
             <Navbar.Brand>Wikipedia</Navbar.Brand>
+            <img
+                src={WikipediaLogo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="Wikipedia logo"
+            />
+
 
         </Navbar>
     )
